13a05fddf36b061b51b1494b7c5f6f178d8037cd
이형진 <hyeongjin3@hanmail.net>
2022-04-30 21:38:13 +0900

    fix(TodoList) : printTodos함수에서 todosContent.text 객체에 생기는 오류 고침

--- src/js/todoList.js
+++ src/js/todoList.js
@@ -3,11 +3,28 @@ const main = document.querySelector("main");
 const todoForm = document.querySelector("#todo-form");
 const todoFormInput = document.querySelector("#todo-form > input[type=text]");
 const todoList = document.querySelector("#todo-list");
+const todoListData = [];
 
 if (main.classList.contains(HIDDEN_CLASS))
 	main.classList.remove(HIDDEN_CLASS);
 
-const printTodos = (newContent) => {
+const saveTodoList = () => {
+
+	if (todoListData.length !== 0)
+	{
+		const stringifiedTodoList = JSON.stringify(todoListData);
+		localStorage.setItem(TODO_LIST_KEY, stringifiedTodoList);
+	}
+}
+
+const deleteTodos = (event) => {
+	const targetLi = event.target.parentNode;
+
+	targetLi.remove();
+	saveTodoList();
+}
+
+const appendList = (newContent) => {
 	const newLi = document.createElement("li");
 	const newSpan = document.createElement("span");
 	const successButton = document.createElement("button");
@@ -16,19 +33,36 @@ const printTodos = (newContent) => {
 	newSpan.innerText = newContent;
 	deleteButton.innerText = "❌";
 	successButton.innerText = "✅";
+	deleteButton.addEventListener("click", deleteTodos);
 	newLi.appendChild(newSpan);
 	newLi.appendChild(successButton); 
 	newLi.appendChild(deleteButton);
 	todoList.appendChild(newLi);
 }
 
+const printTodos = (newContent) => {
+	let todosContent = {};
+
+	appendList(newContent);
+	todosContent.text = newContent;
+	todoListData.push(todosContent);
+	saveTodoList();
+}
+
 const handleSubmitForm = (event) => {
-	const newContent = todoFormInput.value;
 	event.preventDefault();
+	const newContent = todoFormInput.value;
 	printTodos(newContent);
 	todoFormInput.value = "";
 }
 
+const loadTodos = () => {
+	const loadedTodos = localStorage(ge)
+}
 
+const init = () => {
+	// loadTodos();
+	todoForm.addEventListener("submit", handleSubmitForm);
+}
 
-todoForm.addEventListener("submit", handleSubmitForm);
+init();
